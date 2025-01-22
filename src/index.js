b0c3d36d5cd76909b4ace53df0a26d2ea48d18e3
UrsuTudor <tudor_ursu90@yahoo.com>
2025-01-22 13:34:53 +0200

    Add menu_content.js with menuLoad() function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles.css"
 import { pageLoad } from "./main_content.js"
+import { menuLoad } from "./menu_content.js"
 
 pageLoad()
+menuLoad()
---
+++ src/menu_content.js
@@ -0,0 +1,40 @@
+function menuLoad() {
+  const content = document.querySelector("#content")
+
+  const menuItems = [
+    {
+      name: 'Capricciosa',
+      weight: '665g',
+      toppings: 'tomato sauce, mozzarella, ham, mushrooms, corn, olives, peppers',
+    },
+    {
+      name: 'Prosciutto Funghi',
+      weight: '560g',
+      toppings: 'tomato sauce, mozzarella, ham, mushrooms',
+    },
+    {
+      name: 'Quattro Formaggi',
+      weight: '540g',
+      toppings: 'tomato sauce, mozzarella, gorgonzola, parmesan, cedar',
+    },
+    {
+      name: 'Canibale',
+      weight: '660g',
+      toppings: 'tomato sauce, mozzarella, prosciutto, salami, bacon, olives, corn',
+    },
+  ];
+  
+
+  menuItems.forEach(item => {
+    const pizzaHTML = `
+    <div class="menu-item">
+      <h2>${item.name}</h2>
+      <p><strong>Weight:</strong> ${item.weight}</p>
+      <p><strong>Toppings:</strong> ${item.toppings}</p>
+    </div>
+  `;
+    content.insertAdjacentHTML('beforeend', pizzaHTML)
+  })
+}
+
+export {menuLoad}
